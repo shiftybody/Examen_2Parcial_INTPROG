9625e4d523dfb04b7f73dabc368ce08fea3469fb
shiftybody <shiftybody@gmail.com>
2020-11-09 07:19:49 -0600

    creamos requisitos de solucion

---
+++ Examen_2Parcial_INTPROG/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_2Parcial_INTPROG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
